ef33148e1931f6df68589a4d5d52fb07743f5db7
AlikDnepr <oleh.shlykov@gmail.com>
2023-07-01 21:15:00 +0300

    new

--- qa_python/5/home_work_lecture_6_2.py
+++ qa_python/5/home_work_lecture_6_2.py
@@ -14,8 +14,7 @@ def read_from_file(file_path):
 
 
 def clean_text(text):
-    return sorted(
-        text.lower().replace(",", "").replace(".", "").replace("!", "").replace("?", "").split())
+    return sorted(text.lower().replace(",", "").replace(".", "").replace("!", "").replace("?", "").split())
 
 
 def max_word(cleaned_text):
@@ -31,8 +30,9 @@ def min_word(cleaned_text):
 def replace(text_list, max_word, min_word):
     return [word if word != max_word else min_word for word in text_list]
 
+
 def replace_spaces_with_paragraphs(text):
-    words = text.split(" ")
+    words = text.replace("\n", "").split(" ")
     result = ""
     for i, word in enumerate(words):
         if (i + 1) % 10 == 0:
@@ -41,12 +41,21 @@ def replace_spaces_with_paragraphs(text):
             result += word
         if i != len(words) - 1:
             result += " "
-    return result
+    result = result.replace(" \n", "\n").replace("\n ", "\n")  # Удаление пробела перед второй и последующими строками
+    return result.strip()
+
 
 def write_to_file(join_by_ten):
     try:
-        with open("new file.txt", "a") as file:
+        with open("new file.txt", "x") as file:
             return file.writelines(join_by_ten)
+    except FileExistsError:
+        user_input = input("Файл уже существует перезаписать? Возможные ответы Y/N: ")
+        if user_input.lower() == "y":
+            with open("new file.txt", "w") as file:
+                return file.writelines(join_by_ten)
+        else:
+            print("Файл не перезаписан")
     except FileNotFoundError:
         with open("new file.txt", "w") as file:
             return file.writelines(join_by_ten)
@@ -69,6 +78,7 @@ def main():
     new_text = replace_spaces_with_paragraphs(replaced_text)
     write_to_file(new_text)
     print(new_text)
+
 
 """/Users/oshlymeta.ua_1/PycharmProjects/PythonTesting/qa_python/5/example1.txt"""
 if __name__ == "__main__":
